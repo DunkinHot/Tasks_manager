5bc6bfa1337c4f6ec8d7de47ca2509ebccc5dd06
Nniikkoollaass <nikk1.endurable7@gmail.com>
2024-01-27 21:34:23 +0200

    seventh_task_done

--- Task.py
+++ Task.py
@@ -50,3 +50,30 @@ def delete_task_by_name(all_tasks: [], task_name: str):
                 print(f"- task name: {deleted_task[0]}, \n - task description: {deleted_task[1]}, \n - date task: {deleted_task[2]}, \n - task deadline: {deleted_task[3]}")
             task_number+=1
 
+# seventh task Пріоритет тасок: Додайте пріоритет до кожної таски (Високий, Середній, Низький)
+# Напишіть функцію, яка виводить усі таски з Високим пріоритетом
+# Напишіть функцію, яка виводить усі таски з Середнім пріоритетом
+# Напишіть функцію, яка виводить усі таски з Низьким пріоритетом 
+# task_priorities=["Low", "Medium", "High"]      
+    def add_priority_to_task(all_tasks: [], task_name: str, task_priority: str):
+        if all_tasks is not None:
+            for task in all_tasks:
+                if task is not None \
+                and len(task)<5 \
+                and task_name is not None \
+                and task[0]==task_name \
+                and task_priority is not None:
+                    task.append(task_priority)
+    
+    def show_tasks_with_some_priority(all_tasks: [], task_priority: str):
+        if all_tasks is not None:
+            tasks_with_some_priority=[]
+            for task in all_tasks:
+                if task is not None \
+                and len(task)==5 \
+                and task_priority is not None \
+                and task[4]==task_priority:
+                    tasks_with_some_priority.append(task)
+            if tasks_with_some_priority!=None:
+                print(f"Tasks with {task_priority} priority -> \n")
+                print(*tasks_with_some_priority, sep="\n")
