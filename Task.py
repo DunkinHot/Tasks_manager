993e3038494f66fcdac464b8a8c14a01e4e80074
Vitaliy Kohan <C:/Users/Usuario>
2024-01-27 20:07:52 +0200

    task3

--- Task.py
+++ Task.py
@@ -1,3 +1,7 @@
-import re
-from datetime import datetime
-from math import ceil
+
+
+
+def view_all_tasks():
+    print("Всі таски:")
+    for i, task in enumerate(tasks):
+        print(f"#{i + 1}: {task['title']}")
